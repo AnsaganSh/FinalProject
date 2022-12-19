3324b5ba233adb8ed318fd8a4aa4bc8edd29fa1c
AnsaganSh <210103204@stu.sdu.edu.kz>
2022-12-20 02:02:13 +0600

    update index7.php to AnsaganSh

--- index7.php
+++ index7.php
@@ -62,7 +62,50 @@
 
 </header>
 
+    <div class="flex-container1">
 
+
+        <div id="box1">
+            <img id='boxImg' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcCyeczU1cLqV6XZ9Ib9zZHf2ymt4QUHl1LA&usqp=CAU'>
+            <p id='boxTitle'>Кара жорга:<br> каражорга; каз. қара жорға, «вороной иноходец»</p>
+            <span id='boxText'>казахский народный танец и кюй, под аккомпанемент которого он исполняется. У киргизов подобный танец называется кара́ жорго́.</span>
+            <br>
+
+        </div>
+        <div id="box2">
+
+            <img id='boxImg' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkiPU8Y0BdBZSf6-BhJ2o4cEOPVBHILEEeAQ&usqp=CAU'>
+            <p id='boxTitle'> Камажай<br> нарциональный танец</p>
+            <span id='boxText'>
+                Казахский танец «Камажай» основан на древнем культе змеи, поэтому движения танцовщицы имитируют движения змеи. </span>
+            <br>
+
+
+        </div>
+
+    </div>
+    <div class="flex-container2">
+        <div id="box1">
+            <img id='boxImg' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSau9y2rHYdk3S5Clkd3cbd7VevhNDk6ma-Jg&usqp=CAU'>
+            <p id='boxTitle'>Балбырауын <br> нарциональный танец</p>
+            <span id='boxText'>«Балбырауын» («Балбрауын») — казахский сценический танец, исполняемый в сопровождении одноимённого кюя Курмангазы. </span>
+            <br>
+
+        </div>
+        <div id="box2">
+
+            <img id='boxImg' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeVZtWY0CfxymPpvI_UyBuSnzpymEXdmOHlQ&usqp=CAU'>
+            <p id='boxTitle'> Буркит-би<br> нарциональный танец</p>
+            <span id='boxText'>
+                Широко распространен среди казахов, проживающих в Монголии. Характерные движения танца — акцентированные удары носком с раздельным опусканием пятки, резкие повороты головы, отрывистое вытягивание шеи вперед и втягивание к себе. Руки ритмично поднимаются и опускаются, имитируя взмахи крыльев орла.</span>
+            <br>
+
+
+            
+
+        </div>
+
+    </div>
 </body>
 
 </html>
